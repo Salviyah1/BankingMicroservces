c7d07d9de9de2a0506af0d041196156fdc963c38
Cloud User <azureuser@VM04.51gtjd25d40etacqadzf2z1khe.rx.internal.cloudapp.net>
2024-11-19 00:49:39 -0500

    added new files

--- src/test/java/com/example/AppTest.java
+++
@@ -1,9 +0,0 @@
-package com.bank; 
-import org.junit.jupiter.api.Test; 
-import org.springframework.boot.test.context.SpringBootTest; 
-@SpringBootTest 
-class AppTest { 
-    @Test 
-    void contextLoads() { 
-    } 
-} 
